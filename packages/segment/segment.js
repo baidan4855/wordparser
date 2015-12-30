5117e52bcee1c15739247954385b317c691efe24
蒋凯 <kai.jiang@digitalpublishing.cn>
2015-12-30 20:09:43 +0800

    命中次数根据分词结果取得

--- packages/segment/segment.js
+++ packages/segment/segment.js
@@ -1,36 +1,28 @@
 Segment = Npm.require('segment');
 
 var _segment;
-Meteor.startup(function(){
+Meteor.startup(function () {
     _segment = new Segment();
     _segment.useDefault();
 })
 
 
 Meteor.methods({
-    'segment':function(str,options){
-        if(!str)
+    'segment': function (str, options) {
+        if (!str)
             return [];
-        var segs = _segment.doSegment(str,options);
-        if(!_.isEmpty(segs)){
-            var uniqueArr = function(arr) {
-                var res = [], hash = {};
-                for(var i=0, elem; (elem = arr[i]) != null; i++)  {
-                    if (!hash[elem.w])
-                    {
-                        res.push(elem);
-                        hash[elem.w] = true;
-                    }
-                }
-                return res;
+        var segs = _segment.doSegment(str, options);
+        if (!_.isEmpty(segs)) {
+            var hash = {};
+            for (var i = 0, elem; (elem = segs[i]) != null; i++) {
+                var curr = hash[elem.w] || 0;
+                hash[elem.w] = curr + 1
             }
-
-            segs= uniqueArr(segs,"w");
-            _.each(segs,function(seg){
-                seg.h=str.split(seg.w).length-1;
+            segs = _.map(hash, function (v, k) {
+                return {w: k, h: v}
             })
-            segs=_.sortBy(segs,function(au){
-                return 0-au.h;
+            segs = _.sortBy(segs, function (au) {
+                return 0 - au.h;
             })
         }
         return segs;
